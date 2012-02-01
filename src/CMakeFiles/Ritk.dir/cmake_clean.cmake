e2b398c58015ec1340514605c5bd0f169fe03e34
shrinidhi <shrik@shrik-Latitude-E6400.(none)>
2012-02-01 17:43:57 -0500

    first commit; includes antsRegistration

---
+++ src/CMakeFiles/Ritk.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Ritk.dir/src/antsRegistration.cxx.o"
+  "CMakeFiles/Ritk.dir/src/antsCommandLineParser.cxx.o"
+  "CMakeFiles/Ritk.dir/src/antsCommandLineOption.cxx.o"
+  "Ritk.pdb"
+  "Ritk.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Ritk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
